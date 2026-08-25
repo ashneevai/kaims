cb2a43c3b5cc3ffed89a5d498e6db4ecad521808
ashneevai <35860118+ashneevai@users.noreply.github.com>
2026-08-25 07:10:18 +0530

    Wire global Simple and Detail experience modes

--- frontend/react/src/main.jsx
+++ frontend/react/src/main.jsx
@@ -1,13 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import ExperienceShell from "./experience/ExperienceShell";
 import "./styles.css";
 import "./datamatics-base.css";
 import "./datamatics-light.css";
 import "./datamatics-dark.css";
+import "./experience/experience.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <ExperienceShell>
+      <App />
+    </ExperienceShell>
   </React.StrictMode>
 );
